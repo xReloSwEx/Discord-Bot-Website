69a162e19fdd0bbf47be9abbdc5e588bf9c2b525
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 10:29:08 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -13,6 +13,12 @@ export default function Partners() {
                 </h1>
             </div>
 
+            <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Yakında...
+                </h1>
+            </div>
+
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
                 </div>
