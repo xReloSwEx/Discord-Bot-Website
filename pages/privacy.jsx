b57a0b1703f487ba5371f1ff3ed4a39840c01fff
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 10:36:29 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,17 +5,17 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Astra botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir. <a className="text-white">(MESAJLARI OKUYUN, MESAJ GÖNDERİN, BAĞLANTILARI YERLEŞTİRİN, DOSYA EKLE, MESAJ GEÇMİŞİNİ OKUYUN, BAĞLANIN, KONUŞUN, SES AKTİVİTESİNİ KULLANIN, ÖNCELİKLİ KONUŞMACI)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Bot komutlarından belirli komutları kullanırken <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">ReloX</a> botuna oy vermeniz gerekiyor.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the astra bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı ReloX Botu zarar verme kastı olmadığı sürece dilediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Astra.</p>   
+        <p>⊳ ReloX'u kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ Astra bot does not save any data based on users.</p>
+        <p>⊳ ReloX bot, kullanıcılara dayalı hiçbir veriyi kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Botu kullanırken kullanıcı kimliğiniz bir defaya mahsus olmak üzere 60 saniye süreyle sisteme kaydedilip silinir.</p> 
     
         </div>
             </div>
