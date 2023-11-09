90dedfc2ee1a42383707ba3d9cdf293d2ef5bfe7
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 21:08:47 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="ReloX bot ile kendi sunucunuzu yönetmeye ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi sunucunuzu oluşturun, bu keyfi en iyi bot ile yaşayın."
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@ReloX" />
@@ -26,7 +26,7 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="ReloX bot ile kendi sunucunuzu yönetmeye ne dersiniz? Kendi özgür dünyanızı yaratın ve kendi sunucunuzu oluşturun, bu keyfi en iyi bot ile yaşayın."
           />
           <meta property="og:image" content="/img/relox.PNG" />
           <meta property="og:image:alt" content="ReloX" />
