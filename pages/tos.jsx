ccbefe45c949835aae3464f4edfdfb0f8a456590
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 10:50:19 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,19 +2,19 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 9/11/2023 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Her discord sunucusu ReloX botunu ücretsiz olarak kullanabilir, ücret karşılığında satışı veya pazarlaması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Açık kaynak kodlarını diğer sunuculara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Açık kaynak kodlarını başka platformlar üzerinden satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken trol amaçlı, diğer kullanıcılara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
+<p>⊳ Belirli discord bot komutlarını kullanabilmek için belirli aralıklarla <a className="text-white" href="https://top.gg/bot/relox/vote">ReloX</a> botuna oy vermeniz gerekiyor.</p>
 </div>
     </div>
   );
