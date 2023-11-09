0aba4367e7187fe09cbae1f524108c2fd4beb180
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 10:14:52 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -86,7 +86,7 @@ Vote ReloX</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://media.discordapp.net/attachments/1169358873287733308/1172070718482157578/ssa2.PNG?ex=655efaeb&is=654c85eb&hm=b59e5c63f259b2bb622bcd211c7dcd48689259f2d452ad20ca64aae17f555ecc&="  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Botunuzu En İyi Şekilde Kullanın</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel bot ReloX ile en güzel partileri vermeye hazır mısınız?</p>
                 </div>
