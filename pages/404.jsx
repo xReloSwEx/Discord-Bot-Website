23fb2f809214e21f47fa8e1fe5415bd9bb35e07f
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-09 08:38:57 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
-        'Have you lost your way, kiddo?',
-        'Damn it! This dead-end road.',
-        'The Aurors blocked this road, Harry!'
+        'Yolunu mu kaybettin evlat?',
+        'Kahretsin! Bu çıkmaz yol.',
+        'Siherbazlar bu yolu kapattılar, Harry!'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
