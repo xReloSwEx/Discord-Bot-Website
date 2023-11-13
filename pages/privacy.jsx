e6a8f797858c3d0e3a7ef4e027203acb373ca501
xReloSwEx <128875573+xReloSwEx@users.noreply.github.com>
2023-11-13 11:10:01 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for ReloX</h1>
             <p className="text-sm text-white/30 mb-4">Son Güncelleme: 9/11/2023  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ Astra botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir. <a className="text-white">(MESAJLARI OKUYUN, MESAJ GÖNDERİN, BAĞLANTILARI YERLEŞTİRİN, DOSYA EKLE, MESAJ GEÇMİŞİNİ OKUYUN, BAĞLANIN, KONUŞUN, SES AKTİVİTESİNİ KULLANIN, ÖNCELİKLİ KONUŞMACI)</a></p>
+        <p>⊳ ReloX botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir. <a className="text-white">(MESAJLARI OKUYUN, MESAJ GÖNDERİN, BAĞLANTILARI YERLEŞTİRİN, DOSYA EKLE, MESAJ GEÇMİŞİNİ OKUYUN, BAĞLANIN, KONUŞUN, SES AKTİVİTESİNİ KULLANIN, ÖNCELİKLİ KONUŞMACI)</a></p>
         <div className="py-3"></div>
         <p>⊳ Bot komutlarından belirli komutları kullanırken <a className="text-white" href="https://top.gg/bot/relox/vote">ReloX</a> botuna oy vermeniz gerekiyor.</p>
         <div className="py-3"></div>
